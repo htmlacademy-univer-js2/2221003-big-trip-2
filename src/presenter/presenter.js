d7cb6e28be2dd421a5fabecc140d3c9a329a54d1
Vadim Roshchin <Vadim.Roshchin@urfu.me>
2023-03-19 16:26:51 +0500

    OpenAndCloseEditForm

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -24,10 +24,49 @@ export default class TripPresenter {
 
     render(new SortView(), this.#tripContainer);
     render(this.#pointsList, this.#tripContainer);
-    render(new EditFormView(this.#tripPoints[0]), this.#pointsList.element);
+    // render(new EditFormView(this.#tripPoints[0]), this.#pointsList.element);
 
     for (let i = 0; i < this.#tripPoints.length; i++) {
-      render(new WaypointView(this.#tripPoints[i]), this.#pointsList.element);
+      this.#renderPoints(this.#tripPoints[i]);
     }
+  }
+
+  #renderPoints = (point) => {
+    const pointComponent = new WaypointView(point);
+    const pointEditComponent = new EditFormView(point);
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceFormToPoint();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
+    const replacePointToForm = () => {
+      this.#pointsList.element.replaceChild(pointEditComponent.element, pointComponent.element);
+    };
+
+    const replaceFormToPoint = () => {
+      this.#pointsList.element.replaceChild(pointComponent.element, pointEditComponent.element);
+    };
+
+    pointComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      replacePointToForm();
+      document.addEventListener('keydown', onEscKeyDown);
+    });
+
+    pointEditComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      replaceFormToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    pointEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      replaceFormToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    render(pointComponent, this.#pointsList.element)
   }
 }
