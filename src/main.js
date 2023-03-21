947ff58ae9699a36205c06a3bf1e12dc209851b7
Vadim Roshchin <Vadim.Roshchin@urfu.me>
2023-03-21 20:20:11 +0500

    3.2 no-point-view

--- src/main.js
+++ src/main.js
@@ -6,9 +6,10 @@ import PointsModel from './model/points-model.js';
 const siteMainElement = document.querySelector('.page-main');
 const siteHeaderElement = document.querySelector('.trip-main');
 
-const tripPresenter = new TripPresenter(siteMainElement.querySelector('.trip-events'));
 const pointModel = new PointsModel();
+const tripPresenter = new TripPresenter(siteMainElement.querySelector('.trip-events'), pointModel);
+
 
 render(new FilterView(), siteHeaderElement.querySelector('.trip-controls__filters'));
 
-tripPresenter.init(siteMainElement.querySelector('.trip-events'), pointModel);
+tripPresenter.init();
