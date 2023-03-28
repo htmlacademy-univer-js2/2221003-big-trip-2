cffb5017ea45c18758986399b6e81b85dcf1c1a4
Vadim Roshchin <Vadim.Roshchin@urfu.me>
2023-03-28 18:09:16 +0500

    pravka

--- src/view/no-point-view.js
+++ src/view/no-point-view.js
@@ -1,7 +1,7 @@
 import { createElement } from '../render';
 
 const createNoPointTemplate = () => (
-  `<p class="trip-events__msg">Click New Event to create your first point</p>`
+  '<p class="trip-events__msg">Click New Event to create your first point</p>'
 );
 
 export default class NoPointView {
