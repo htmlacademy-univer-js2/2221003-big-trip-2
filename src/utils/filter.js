ef5a9ede7aaa4043084eeaf2a69b075182c7fafe
Vadim Roshchin <Vadim.Roshchin@urfu.me>
2023-04-03 19:01:12 +0500

    module4task2

---
+++ src/utils/filter.js
@@ -0,0 +1,10 @@
+import { FilterType } from '../mock/constants';
+import { isPointFuture, isPointPast } from './point';
+
+const filter = {
+  [FilterType.EVERYTHING]: (points) => Array.from(points),
+  [FilterType.FUTURE]: (points) => Array.from(points).filter((point) => isPointFuture(point)),
+  [FilterType.PAST]: (points) => Array.from(points).filter((point) => isPointPast(point))
+};
+
+export { filter };
