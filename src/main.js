be37f580b8556b43c40d48ec1a12349be573db69
Vadim Roshchin <Vadim.Roshchin@urfu.me>
2023-06-15 12:47:22 +0500

    q

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { render } from './render.js';
+import { render } from './framework/render.js';
 import TripPresenter from './presenter/board-presenter.js';
 import PointsModel from './model/points-model.js';
 
